5c92a3f8411857c4b88f37e2aea5e565268ef2c8
MendicantBias-096 <a.m.jacomep.test@gmail.com>
2024-01-29 02:17:02 -0600

    feat: Se completo el desafio de la clase 03 Loops y tentativas

---
+++ 01 Lógica de programación/03 Loops y tentativas/desafio3.js
@@ -0,0 +1,42 @@
+// Ejercicio 1
+// let contador = 1;
+
+// alert('¿Comenzamos?');
+
+// while (contador <= 10) {
+//     alert('El contador actualmente es igual a: ' + contador);
+//     contador++;
+// }
+
+// Ejercicio 2
+
+// let contador = 10;
+
+// alert('¿Comenzamos?');
+
+// while (contador <= 10 && contador >= 0) {
+//     alert('El contador actualmente es igual a: ' + contador);
+//     contador--;
+// }
+
+// Ejercicio 3
+
+// let numeroUsuario = prompt('Dame un numero para la cuenta progresiva');
+// let contador = 0;
+
+// while (contador <= numeroUsuario) {
+//     console.log('Cuenta progresiva: ' + contador);
+//     contador++;
+// }
+
+// Ejercicio 4
+
+let numeroUsuario = prompt('Dame un numero para la cuenta regresiva');
+
+while (numeroUsuario >= 0) {
+    console.log('Cuenta regresiva: ' + numeroUsuario);
+    numeroUsuario--;
+}
+
+
+
