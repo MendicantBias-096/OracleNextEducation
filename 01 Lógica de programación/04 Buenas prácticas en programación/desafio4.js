410c6abb2505d83da61d7b9bcd888a2008d0a4b5
MendicantBias-096 <a.m.jacomep.test@gmail.com>
2024-01-29 02:23:18 -0600

    feat: Se completo el desafio de la clase 04 Buenas prácticas en programación

---
+++ 01 Lógica de programación/04 Buenas prácticas en programación/desafio4.js
@@ -0,0 +1,68 @@
+// Ejercicio 1
+// console.log('Bienvenido al juego del número');
+
+// Ejercicio 2
+// let nombre = 'Arturo';
+// console.log('¡Hola, ' + nombre + '!');
+
+// Ejercicio 3
+// let nombre = 'Arturo';
+// alert('¡Hola, ' + nombre + '!');
+
+// Ejercicio 4
+// let respuesta = prompt('¿Cuál es el lenguaje de programacion que más te gusta?');
+// console.log(respuesta);
+
+// Ejercicio 5
+// let valor1 = 5;
+// let valor2 = 7;
+// let resultado = valor1 + valor2;
+// console.log('La suma de ' + valor1 + ' + ' + valor2 + ' es igual a ' + resultado);
+
+// Ejercicio 6
+// let valor1 = 5;
+// let valor2 = 7;
+// let resultado = valor1 - valor2;
+// console.log('La suma de ' + valor1 + ' - ' + valor2 + ' es igual a ' + resultado);
+
+// Ejercicio  7
+// let edad = prompt('Ingresa tu edad');
+// if (edad >= 18) {
+//     console.log('Eres mayor de edad');
+// } else {
+//     console.log('Eres menor de edad');
+// }
+
+// Ejercicio 8
+// let numero = prompt('Ingresa un número');
+// if (numero > 0) {
+//     console.log('Tu numero es positivo');
+// } else if (numero < 0) {
+//     console.log('Tu numero es negativo');
+// } else if (numero == 0){
+//     console.log('Tu numero es cero');
+// }
+
+// Ejercicio 9
+// let numero = 1;
+// while (numero <= 10) {
+//     console.log(numero);
+//     numero++;
+// }
+
+// Ejercicio 10
+// let nota = 13;
+// if (nota >= 7) {
+//     console.log('Aprovado');
+// } else {
+//     console.log('Reprobado');
+// }
+
+// Ejercicio 11
+// console.log(Math.random());
+
+// Ejercicio 12
+// console.log(Math.floor(Math.random() * 10));
+
+// Ejercicio 13
+console.log(Math.floor(Math.random()*1000));
